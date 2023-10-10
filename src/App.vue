40ebe6e80085eb4fe47df8c027098a1db74e3f12
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-10-10 13:55:04 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+
+
+export default {
+  name:'App',
+  components: {
+
+  }
+
+}
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style lang="scss">
+
+@use './scss/main.scss';
+
+
+
+</style>
